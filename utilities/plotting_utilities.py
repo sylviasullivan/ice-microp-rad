73fd32e7fbe11171fe62f253dd4365304f31e927
sylviasullivan <sylvia.c.sullivan@gmail.com>
2021-09-19 23:23:20 +0200

    new utilities files

--- utilities/plotting_utilities.py
+++ utilities/plotting_utilities.py
@@ -15,6 +15,12 @@ def sim_ls():
     return {'1V': '-', '0V': '--', 'CE': '-', 'ER': '-'}
 
 
+# Generally define a file prefix for a set of 0s of length <n>
+def general_prefix(j, n):
+    # How many zeros do we need?
+    m = n - len(str(j))
+    return '0' * m + str(j)
+
 # Read in the ICON simulation output files from the file number
 # This function prepends the appropriate number of zeros
 def file_prefix(j):
@@ -80,3 +86,132 @@ def resize_colorbar(event):
     cbar_ax.set_position([posn.x0 + posn.width + 0.01, posn.y0 - 0.018,
                           0.025, posn.height + 0.03])
     cbar_ax.tick_params(labelsize=fs)
+
+
+# A solution to plot seaborn jointplots in different subplots of a matplotlib fig
+# https://stackoverflow.com/questions/35042255/how-to-plot-multiple-seaborn-jointplot-in-subplot
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+import seaborn as sns
+import numpy as np
+
+class SeabornFig2Grid():
+
+    def __init__(self, seaborngrid, fig,  subplot_spec):
+        self.fig = fig
+        self.sg = seaborngrid
+        self.subplot = subplot_spec
+        if isinstance(self.sg, sns.axisgrid.FacetGrid) or \
+            isinstance(self.sg, sns.axisgrid.PairGrid):
+            self._movegrid()
+        elif isinstance(self.sg, sns.axisgrid.JointGrid):
+            self._movejointgrid()
+        self._finalize()
+
+    def _movegrid(self):
+        """ Move PairGrid or Facetgrid """
+        self._resize()
+        n = self.sg.axes.shape[0]
+        m = self.sg.axes.shape[1]
+        self.subgrid = gridspec.GridSpecFromSubplotSpec(n,m, subplot_spec=self.subplot)
+        for i in range(n):
+            for j in range(m):
+                self._moveaxes(self.sg.axes[i,j], self.subgrid[i,j])
+
+    def _movejointgrid(self):
+        """ Move Jointgrid """
+        h= self.sg.ax_joint.get_position().height
+        h2= self.sg.ax_marg_x.get_position().height
+        r = int(np.round(h/h2))
+        self._resize()
+        self.subgrid = gridspec.GridSpecFromSubplotSpec(r+1,r+1, subplot_spec=self.subplot)
+
+        self._moveaxes(self.sg.ax_joint, self.subgrid[1:, :-1])
+        self._moveaxes(self.sg.ax_marg_x, self.subgrid[0, :-1])
+        self._moveaxes(self.sg.ax_marg_y, self.subgrid[1:, -1])
+
+    def _moveaxes(self, ax, gs):
+        #https://stackoverflow.com/a/46906599/4124317
+        ax.remove()
+        ax.tick_params('both',labelsize=18)
+        ax.figure=self.fig
+        self.fig.axes.append(ax)
+        self.fig.add_axes(ax)
+        ax._subplotspec = gs
+        ax.set_position(gs.get_position(self.fig))
+        ax.set_subplotspec(gs)
+
+    def _finalize(self):
+        plt.close(self.sg.fig)
+        self.fig.canvas.mpl_connect("resize_event", self._resize)
+        self.fig.canvas.draw()
+
+    def _resize(self, evt=None):
+        self.sg.fig.set_size_inches(self.fig.get_size_inches())
+
+
+# Function to create a boxplot from 5 percentiles, taken from
+# https://stackoverflow.com/questions/27214537/is-it-possible-to-draw-a-matplotlib-boxplot-given-the-percentile-values-instead
+# sylvia made n_box and pos function inputs
+def customized_box_plot(percentiles, axes, n_box, pos, redraw = True, *args, **kwargs):
+    """
+    Generates a customized boxplot based on the given percentile values
+    """
+
+    box_plot = axes.boxplot([[-9, -4, 2, 4, 9],]*n_box, positions=pos, *args, **kwargs)
+    # Creates len(percentiles) no of box plots
+
+    min_y, max_y = float('inf'), -float('inf')
+
+    for box_no, (q1_start,
+                 q2_start,
+                 q3_start,
+                 q4_start,
+                 q4_end,
+                 fliers_xy) in enumerate(percentiles):
+
+        # Lower cap
+        box_plot['caps'][2*box_no].set_ydata([q1_start, q1_start])
+        # xdata is determined by the width of the box plot
+
+        # Lower whiskers
+        box_plot['whiskers'][2*box_no].set_ydata([q1_start, q2_start])
+
+        # Higher cap
+        box_plot['caps'][2*box_no + 1].set_ydata([q4_end, q4_end])
+
+        # Higher whiskers
+        box_plot['whiskers'][2*box_no + 1].set_ydata([q4_start, q4_end])
+
+        # Box
+        box_plot['boxes'][box_no].set_ydata([q2_start,
+                                             q2_start,
+                                             q4_start,
+                                             q4_start,
+                                             q2_start])
+
+        # Median
+        box_plot['medians'][box_no].set_ydata([q3_start, q3_start])
+
+        # Outliers
+        if fliers_xy is not None and len(fliers_xy[0]) != 0:
+            # If outliers exist
+            box_plot['fliers'][box_no].set(xdata = fliers_xy[0],
+                                           ydata = fliers_xy[1])
+
+            min_y = min(q1_start, min_y, fliers_xy[1].min())
+            max_y = max(q4_end, max_y, fliers_xy[1].max())
+
+        else:
+            min_y = min(q1_start, min_y)
+            max_y = max(q4_end, max_y)
+
+        # The y axis is rescaled to fit the new box plot completely with 10%
+        # of the maximum value at both ends
+        axes.set_ylim([min_y*1.1, max_y*1.1])
+
+    # If redraw is set to true, the canvas is updated.
+    if redraw:
+        axes.figure.canvas.draw()
+
+    return box_plot
--- utilities/thermodynamic_functions.py
+++ utilities/thermodynamic_functions.py
@@ -1,7 +1,29 @@
+import numpy as np
+
+# Function to calculate the saturation vapor pressure over liquid.
+# psatL [=] Pa; t_in [=] K
+def satVapP_liq(t_in):
+    R = 8.314             # J mol-1 K-1
+    MWw = 18.015/1000     # kg mol-1
+    rhoa = 1.395
+    a1 = 54.842763
+    a2 = -6763.22
+    a3 = -4.21
+    a4 = 0.000367
+    a5 = 0.0415
+    a6 = 218.8
+    a7 = 53.878
+    a8 = -1331.22
+    a9 = -9.44523
+    a10 = 0.014025
+    factor = a7 + a8/t_in + a9*np.log(t_in) + a10*t_in
+    psatL = a1 + a2/t_in + a3*np.log(t_in) + a4*t_in + np.arctan(a5*(t_in - a6))*factor
+    psatL = np.exp(psatL)
+    return psatL
+
 # Function to calculate the saturation vapor pressure over ice.
 # psatI [=] Pa and t_in [=] K
 def satVapP_ice(t_in):
-    import numpy as np
     a1 = 9.550426
     a2 = -5723.265
     a3 = 3.53068
@@ -19,14 +41,13 @@ def calc_theta(t_in, p_in):
     return t_in * ( p0 / p_in ) ** ( R_cp )
 
 
-# Function to calculate the relative humidity
-# t_in [=] K, p_in [=] Pa, qv [=] qv_in
-def calc_RH(t_in, p_in, qv_in):
+# Function to calculate the relative humidity with respect to ice
+# t_in [=] K, p_in [=] Pa, qv [=] kg kg-1, return [=] %
+def calc_RHi(t_in, p_in, qv_in):
     eps = 0.622  # ratio of molar mass of moist air to molar mass of dry air
     psat = satVapP_ice(t_in)
     qsat = eps * psat / (p_in - psat)
-    qv = qv_in / 10**6 * eps
-    return qv / qsat * 100
+    return qv_in / qsat * 100
 
 
 # Utility function to calculate running mean on a vector time series x over N time points.
@@ -40,7 +61,7 @@ def running_mean2(x, N, a):
     cumsum = np.cumsum(np.insert(x, 0, 0, axis=a)) 
     #print(cumsum.shape)
     #print(cumsum[:-N].shape)
-    
+ 
     # Make a the first axis for the calculation and then reverse
     if a != 0:
         cumsum = np.swapaxes(cumsum, a, 0)
